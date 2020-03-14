9e42406cd96d480dabf689897c5c966d48cb5a04
mojtaba rostami <mrbilandi@gmail.com>
2020-03-14 20:46:22 +0330

    remove hls 2018.3

--- tdt/solution1/csim/build/obj/aux.d
+++
@@ -1,7 +0,0 @@
-obj/aux.o: ../../../../src/aux.c ../../../../src/aux.h \
- /opt/Xilinx/Vivado/2018.3/include/ap_cint.h \
- /opt/Xilinx/Vivado/2018.3/include/etc/autopilot_apint.h \
- /opt/Xilinx/Vivado/2018.3/include/etc/autopilot_dt.h \
- /opt/Xilinx/Vivado/2018.3/include/etc/autopilot_dt.def \
- /opt/Xilinx/Vivado/2018.3/include/etc/autopilot_dt_ext.def \
- /opt/Xilinx/Vivado/2018.3/include/etc/autopilot_ssdm_bits.h
--- tdt/solution1/csim/build/obj/tdt_tb.d
+++
@@ -1,7 +0,0 @@
-obj/tdt_tb.o: ../../../../src/tdt_tb.c ../../../../src/tdt.h \
- /opt/Xilinx/Vivado/2018.3/include/ap_cint.h \
- /opt/Xilinx/Vivado/2018.3/include/etc/autopilot_apint.h \
- /opt/Xilinx/Vivado/2018.3/include/etc/autopilot_dt.h \
- /opt/Xilinx/Vivado/2018.3/include/etc/autopilot_dt.def \
- /opt/Xilinx/Vivado/2018.3/include/etc/autopilot_dt_ext.def \
- /opt/Xilinx/Vivado/2018.3/include/etc/autopilot_ssdm_bits.h
